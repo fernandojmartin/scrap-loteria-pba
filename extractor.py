2c6a85722df4135e83a392091636a720cfd16fe2
Fernando Javier Martin <fernando@2amigos.us>
2021-11-09 01:39:34 -0300

    Ahora el extractor procesa todos los PDF del directorio recursivamente

--- extractor.py
+++ extractor.py
@@ -1,33 +1,24 @@
+import glob
+
 from finders import get_lotteries_count, get_losers
 from validators import is_quiniela_multiple_format
 from structures import quiniela_multiple_data_matrix
 from dtos import LotteryResults
 import fitz
 
-doc = fitz.open('QuinielaMatutina.pdf')
-page = doc[0]
 
-blocks = list(block[4] for block in page.get_text('blocks'))
-# for i, p in enumerate(blocks): print(i, ' => ', p.strip())
-
-if not is_quiniela_multiple_format(blocks):
-    exit('Formato de extracto no soportado.')
-
-lotteries_count = get_lotteries_count(page)
-
-
-def extract_results(positions):
+def extract_results(positions, blocks):
     return LotteryResults(
         date=blocks[positions.date].strip(),
         lottery_number=blocks[positions.lottery_number].strip(),
         district=blocks[positions.district].strip().upper(),
         shift=blocks[positions.shift].strip(),
-        numbers=extract_lottery_numbers(positions.numbers),
+        numbers=extract_lottery_numbers(positions.numbers, blocks),
         losers=get_losers(blocks, blocks[positions.shift].strip())
     )
 
 
-def extract_lottery_numbers(blocks_range):
+def extract_lottery_numbers(blocks_range, blocks):
     numbers = [premio.strip().split('\n', 1)[1] for premio in blocks[blocks_range]]
     return list(enumerate((number for number in numbers), 1))
 
@@ -43,6 +34,29 @@ def print_results(result):
     print('---------------------------------------')
 
 
-print('Cantidad de sorteos:', lotteries_count)
-for lottery in range(lotteries_count):
-    print_results(extract_results(quiniela_multiple_data_matrix[lottery]))
+def parse_pdf(pdf_path: str):
+    doc = fitz.open(pdf_path)
+    page = doc[0]
+
+    blocks = list(block[4] for block in page.get_text('blocks'))
+    # for i, p in enumerate(blocks): print(i, ' => ', p.strip())
+
+    if not is_quiniela_multiple_format(blocks):
+        print('------------------------------------')
+        print('  Formato de extracto no soportado  ')
+        print('------------------------------------')
+        return
+
+    lotteries_count = get_lotteries_count(page)
+
+    print('Cantidad de sorteos:', lotteries_count)
+    for lottery in range(lotteries_count):
+        print_results(
+            extract_results(quiniela_multiple_data_matrix[lottery], blocks)
+        )
+
+
+files = glob.glob('downloads/**/*.pdf', recursive=True)
+for pdf in files[:10]:
+    print(f'PATH: {pdf}')
+    parse_pdf(pdf)
